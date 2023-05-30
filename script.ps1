dcc6f1adb3fc69960570c2d55f8adb87ab0bacf6
fuzzyBear123 <fuzzybear255@yahoo.com>
2023-05-30 19:53:38 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -30,3 +30,4 @@ Temporary commit content for 2023-04-25T19:53:38
 Temporary commit content for 2023-04-29T19:53:38
 Temporary commit content for 2023-05-02T19:53:38
 Temporary commit content for 2023-05-29T19:53:38
+Temporary commit content for 2023-05-30T19:53:38
