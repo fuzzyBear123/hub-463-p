f89503c1716a5ade3d7a352b4bd3a3a9384dceba
fuzzyBear123 <fuzzybear255@yahoo.com>
2023-07-18 19:53:38 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -33,3 +33,4 @@ Temporary commit content for 2023-05-29T19:53:38
 Temporary commit content for 2023-05-30T19:53:38
 Temporary commit content for 2023-06-03T19:53:38
 Temporary commit content for 2023-06-27T19:53:38
+Temporary commit content for 2023-07-18T19:53:38
